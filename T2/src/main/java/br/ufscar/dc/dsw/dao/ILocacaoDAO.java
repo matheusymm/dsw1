285da31f0b7ecddb83e91491557bd9a7ccc6d329
AndreRetton <andre.retton21@gmail.com>
2024-08-15 15:06:28 -0300

    add: CRUD Locacao

---
+++ T2/src/main/java/br/ufscar/dc/dsw/dao/ILocacaoDAO.java
@@ -0,0 +1,15 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.ufscar.dc.dsw.domain.Cliente;
+import br.ufscar.dc.dsw.domain.Locacao;
+
+@SuppressWarnings("unchecked")
+public interface ILocacaoDAO extends CrudRepository<Locacao, Long>{
+	Locacao findById(long id);
+	List<Locacao> findAllByUsuario(Cliente user);
+	Locacao save(Locacao locacao);
+}
